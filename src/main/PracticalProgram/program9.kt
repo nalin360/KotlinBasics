b28d61ea700f4608be6ebf0c95c3d718e2e59867
Nalin_sahani <45825684+nalin360@users.noreply.github.com>
2023-10-05 14:03:16 +0530

    modified practical 8 and 9

--- src/main/PracticalProgram/program9.kt
+++ src/main/PracticalProgram/program9.kt
@@ -1,25 +1,24 @@
-fun main() {
-    /*
-    * prime number
-    * */
-    val number = 13
-    val isPrime = isPrime(number)
-    if (isPrime) {
-        println("$number is a prime number.")
-    } else {
-        println("$number is not a prime number.")
-    }
-}
-fun isPrime(number: Int): Boolean {
-    if (number <= 1) {
-        return false
+fun main(args: Array<String>) {
+    val primes = mutableListOf<Int>()
+
+    // Check if a number is prime
+    fun isPrime(n: Int): Boolean {
+        for (i in 2 until n) {
+            if (n % i == 0) {
+                return false
+            }
+        }
+        return true
     }
 
-    for (i in 2 until number) {
-        if (number % i == 0) {
-            return false
+    // Iterate through all numbers from 1 to 100 and add all prime numbers to a list
+    for (i in 1..100) {
+        if (isPrime(i)) {
+            primes.add(i)
         }
     }
 
-    return true
+    // Print all prime numbers
+    println("The prime numbers from 1 to 100 are:")
+    println(primes)
 }
